2899e04c9be27703fe72ebd12d8003a8223845db
Blowing <1072878103@qq.com>
2018-08-06 14:12:27 +0800

    Initial commit

---
+++ app/src/main/java/com/wujie/android/voicedemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wujie.android.voicedemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
